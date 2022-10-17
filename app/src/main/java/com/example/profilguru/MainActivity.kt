9906c284629fc28cfba7ad5bddb3a3c3b1c0466f
SahrulHakim <Sahrulhakim895@gmail.com>
2022-10-17 07:44:16 +0700

    first commit

---
+++ app/src/main/java/com/example/profilguru/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.profilguru
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button)
+        val btn2 = findViewById<Button>(R.id.button2)
+        btn2.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        btn.setOnClickListener {
+            val fiki = Intent(this, MainActivity3::class.java)
+            startActivity(fiki)
+        }
+    }
+}
